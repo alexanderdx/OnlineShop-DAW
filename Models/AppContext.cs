9124ab86c3f6e51e0d5d30586394578d507c4706
Vlad Pirlog <avi.pirlog@gmail.com>
2020-11-21 20:59:04 +0200

    :sparkles: Add models

---
+++ Models/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Online_Shop___DAW.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base ("DBConnectionString") {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppContext,
+                    Online_Shop___DAW.Migrations.Configuration>("DBConnectionString"));
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shop___DAW.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public int Rating { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
