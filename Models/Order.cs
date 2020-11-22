7ee61cde154c21c8cd45793c5d84c099f9b79488
Alexandru Dumea <alexandrugabriel.dumea@gmail.com>
2020-11-22 17:08:21 +0200

    💥 Track only the necessary source files

---
+++ Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shop___DAW.Models
+{
+    public enum OrderStatus
+    {
+        not_accepted,
+        accepted,
+        en_route,
+        delivered
+    }
+
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        [Required]
+        public OrderStatus Status { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public virtual ICollection<OrderProduct> OrderProducts { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Online_Shop___DAW.Models
+{
+    public enum ProductStatus
+    {
+        pending,
+        rejected,
+        accepted
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public string Image { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        [Required]
+        public ProductStatus Status { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<OrderProduct> OrderProducts { get; set; }
+    }
+}
