718d882ebcdd14caa649c180a97bb104dc64f9bd
Vlad Pirlog <avi.pirlog@gmail.com>
2020-12-09 21:49:03 +0200

    :zap: Fix ViewBag data in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Online_Shop___DAW.Models;
 
 namespace Online_Shop___DAW.Controllers
 {
     public class HomeController : Controller
     {
+        private Models.AppContext db = new Models.AppContext();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            var categories = from cat in db.Categories orderby cat.Name ascending select cat;
+            var dealsOfTheDay = from prod
+                                in db.Products
+                                where prod.DiscountedPrice != null
+                                    && prod.Status == ProductStatus.accepted
+                                select prod;
+            ViewBag.Categories = categories.ToList();
+            ViewBag.DealsOfTheDay = dealsOfTheDay
+                .Take(10)
+                .OrderByDescending(p => p.Price - p.DiscountedPrice)
+                .ToList();
             return View();
         }
     }
