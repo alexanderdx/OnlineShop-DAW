3eade8abecf05090fd0212d486f207d3ff6dba5a
Vlad Pirlog <avi.pirlog@gmail.com>
2020-12-09 21:42:12 +0200

    :lipstick: Add CSS, JS and image files

---
+++ Content/js/script.js
@@ -0,0 +1,28 @@
+(function($) {
+    $('#carousel-1').carousel({ interval: 3000 })
+    const MD_BREAKPOINT_BOOTSTRAP = 768
+    if (window.innerWidth >= MD_BREAKPOINT_BOOTSTRAP) {
+        const categoryList = $('#category-list')
+
+        const MARGIN_TO_TOP = window.innerWidth * 0.02
+        const INITIAL_LIST_HEIGHT = categoryList.offset().top
+    
+        categoryList.css('position', 'relative');
+    
+        $(window).on('scroll', ev => {
+            if (window.innerWidth < MD_BREAKPOINT_BOOTSTRAP) {
+                categoryList.css('position', '')
+                return
+            }
+            const scrollTop = $(window).scrollTop();
+
+            categoryList.stop(false, false).animate({
+                top: scrollTop < INITIAL_LIST_HEIGHT
+                        ? 0
+                        : scrollTop - INITIAL_LIST_HEIGHT + MARGIN_TO_TOP
+            }, 250);
+        });
+    } else {
+        $('#collapse-1').collapse('hide')
+    }
+})(jQuery);
