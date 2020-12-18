467356cc95ecad5bda9f292a4f06c97dd4dfb402
Alexandru Dumea <alexandrugabriel.dumea@gmail.com>
2020-12-18 11:46:46 +0200

    Add font-awesome icons

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace OnlineShopDAW
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/font-awesome.min.css"));
         }
     }
 }
